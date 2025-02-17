7067bcf1bfff2e4a41f32cc4986547f112863c89
Viktoriia Ursul <viktoriiaursul@gmail.com>
2025-02-17 22:19:35 +0200

    added ejs file

--- lab2/src/main.ts
+++ lab2/src/main.ts
@@ -19,9 +19,24 @@ app.get('/', (request: Request, response: Response) => {
   response.sendFile(__dirname + '/views/index.html');
 });
 
+const members = [
+  { id: "1", name: "Pasha", age: 19, photo: "/pasha.jpg" },
+  { id: "2", name: "Vanya", age: 18, photo: "https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png" },
+  { id: "3", name: "Vika", age: 18, photo: "/vika.jpg" },
+  { id: "4", name: "Oleh", age: 19, photo: "https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png" }
+];
+
+app.set('view engine', 'ejs');
+app.use(express.static('src/styles'));
+app.use(express.static('src/images'));
+
+app.get('/', (request: Request, response: Response) => {
+  response.sendFile(__dirname + '/views/index.html');
+});
+
 app.get('/person/:id', (request: Request, response: Response) => {
-  const personId = request.params.id;
-  response.render(createPath('profile'), {});
+  const person = members.find(s => s.id === request.params.id);
+  response.render(createPath('profile'), {person});
 });
 
 app.use((request: Request, response: Response) => {
