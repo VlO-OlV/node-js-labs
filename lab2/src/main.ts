c7af010dd59387f82072a1d4ffecc706edffdacd
VlO-OlV <linichenko.vanya@gmail.com>
2025-02-17 19:28:59 +0200

    added endpoints

--- lab2/src/main.ts
+++ lab2/src/main.ts
@@ -1,7 +1,10 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import * as dotenv from 'dotenv';
+import * as path from 'path'
 
 const app = express();
+
+const createPath = (page: string) => path.join(__dirname, '/views', `${page}.ejs`);
 
 dotenv.config({
   path: __dirname + '/.env',
@@ -12,8 +15,17 @@ const port = process.env.PORT || 3000;
 app.use(express.static('src/styles'));
 app.use(express.static('src/images'));
 
-app.get('/', (request, response) => {
+app.get('/', (request: Request, response: Response) => {
   response.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('/person/:id', (request: Request, response: Response) => {
+  const personId = request.params.id;
+  response.render(createPath('profile'), {});
+});
+
+app.use((request: Request, response: Response) => {
+  response.status(404).send('Page not found');
 });
 
 app.listen(port, () => console.log(`Running on port ${port}`));
