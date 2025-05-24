6e1658d5f519eb3d8a4de4b197b926c93b262b18
Viktoriia Ursul <viktoriiaursul@gmail.com>
2025-05-24 16:21:47 +0300

    added transactions

--- lab4/src/services/menuService.ts
+++ lab4/src/services/menuService.ts
@@ -5,14 +5,37 @@ export const getAllMenuItems = (): Promise<MenuItem[]> => {
     return db.menuItem.find();
 }
 
-export const getMenuItemById = (id: number): Promise<MenuItem> => {
-    return db.menuItem.findById(id);
+export const getMenuItemById = async (id: number): Promise<MenuItem> => {
+    return db.menuItem.findById(id).then(item => {
+        if (!item) {
+            throw new Error(`Menu item with id ${id} does not exist`);
+        }
+        return item;
+    });
 };
 
-export const createMenuItem = (menuItem: MenuItem): Promise<void> => {
-    return db.menuItem.create(menuItem);
+export const createMenuItem = (menuItem: Omit<MenuItem, 'id'>): Promise<void> => {
+    return db.tx(async t => {
+        await t.menuItem.create(menuItem);
+    });
 };
 
 export const deleteMenuItem = (menuItemId: number): Promise<void> => {
-    return db.menuItem.deleteById(menuItemId);
+    return db.tx(async t => {
+        const item = await t.menuItem.findById(menuItemId);
+        if (!item) {
+            throw new Error(`This item is not found`);
+        }
+        await t.menuItem.deleteById(menuItemId);
+    });
+};
+
+export const updateMenuItem = (id: number, updatedFields: Partial<Omit<MenuItem, 'id'>>): Promise<void> => {
+    return db.tx(async t => {
+        const existing = await t.menuItem.findById(id);
+        if (!existing) {
+            throw new Error('Item not found');
+        }
+        await t.menuItem.updateById(id, updatedFields);
+    });
 };
--- lab4/src/services/orderService.ts
+++ lab4/src/services/orderService.ts
@@ -19,13 +19,31 @@ export const getOrderById = async (id: number): Promise<Order & { items: OrderIt
 };
 
 export const createOrder = (order: Omit<Order, 'id'>): Promise<Order> => {
-    return db.order.create(order);
+    return db.tx(async t => {
+        return await t.order.create(order);
+    });
 };
 
 export const addOrderItem = (orderItem: Omit<OrderItem, 'id'>): Promise<void> => {
-    return db.orderItem.create(orderItem);
+    return db.tx(async t => {
+        const order = await t.order.findById(orderItem.orderId);
+        if (!order) {
+            throw new Error('Order with id ${orderItem.orderId} not found');
+        }
+        const item = await db.orderItem.findById(orderItem.menuItemId);
+        if (!item) {
+            throw new Error('Menu item with id ${orderItem.menuItemId} not found');
+        }
+        await t.orderItem.create(orderItem);
+    });
 };
 
 export const updateOrderStatus = (orderId: number, status: OrderStatus): Promise<void> => {
-    return db.order.updateById(orderId, { status });
-}
+    return db.tx(async t => {
+        const existingOrder = await t.order.findById(orderId);
+        if (!existingOrder) {
+            throw new Error('Order with id ${orderId} not found');
+        }
+        await t.order.updateById(orderId, { status });
+    });
+};
